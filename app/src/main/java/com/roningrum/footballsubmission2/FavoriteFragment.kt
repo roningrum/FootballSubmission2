26860b9bd5d291ee5084f306c5c5015dc8bfb8e0
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2018-10-24 02:43:10 +0700

    Added Favorite Fragment

---
+++ app/src/main/java/com/roningrum/footballsubmission2/FavoriteFragment.kt
@@ -0,0 +1,17 @@
+package com.roningrum.footballsubmission2
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FavoriteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    }
+}
